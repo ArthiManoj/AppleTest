df5c63545a47cd38f840f1cd5a97fe9810a6ae5e
ArthiManoj <arthitech2015@gmail.com>
2016-10-26 16:44:41 -0700

    Initial Commit

---
+++ apple/src/test/java/com/apple/test/apple/TestSuite.java
@@ -0,0 +1,15 @@
+package com.apple.test.apple;
+import org.junit.runner.RunWith;
+import org.junit.runners.Suite;
+
+@RunWith(Suite.class)
+
+@Suite.SuiteClasses({
+   StringManipulationTest.class,
+   ApiTest.class
+   
+})
+
+public class TestSuite {
+
+}
